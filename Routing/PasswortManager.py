9b530d622ed9474abc1a8f71a596959e2a9ef31b
Christian Weiß <weiss-christian@hotmail.de>
2017-07-27 10:01:48 +0200

    Opens a screen to select next action.

--- Routing/PasswortManager.py
+++ Routing/PasswortManager.py
@@ -6,10 +6,10 @@ from Database.Models import Account
 
 @app.route('/')
 def start():
-    pageValues = dict(header='Liste aller Accounts')
+    pageValues = dict(header='Liste aller Accounts', ngApp='AccountList', ngCtrl='ListController')
     return render_template('AccountList.html', pageValues=pageValues)
 
-@app.route('/accountList')
-def getAccountList():
-    list = Account.getObjList(['id', 'provider', 'username', 'password'])
+@app.route('/accountListData')
+def accountListData():
+    list = Account.getObjList(['id', 'provider', 'username'])
     return jsonify(list)
